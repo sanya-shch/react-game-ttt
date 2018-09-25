900a7eb2cd6d9379f3383973f62b9681df7598e0
sanya-shch <shchipkov.sanya@ukr.net>
2018-09-25 17:49:49 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// var arr =[1,2,3,4,5];var rez =arr.filter(function(el){return el % 2;});console.log({rez});
+// var a={name:'a'};var b={name:'a'};console.log(a===b);
+//
+// function f1(a,b) {
+//     console.log(a,b);
+// }
+// var f2=f1.bind(null,'foo');
+// f2('bar','baz');
+
+class App extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            turn: 'X',
+            gameEnded: false,
+            board: Array(9).fill(''),
+            totalMoves: 0,
+            winner: undefined
+        }
+
+    }
+
+    clicked(event){
+        if(this.state.board[event.target.dataset.square] == '') {
+            this.state.board[event.target.dataset.square] = this.state.turn;
+            event.target.innerText = this.state.turn;
+            this.setState({
+                turn: this.state.turn == 'X' ? 'O' : 'X',
+                board: this.state.board,
+                totalMoves: this.state.totalMoves++
+            })
+        }
+
+        var rezult = this.checkWinner();
+        if(rezult == 'X'){
+            this.setState({
+                gameEnded: true,
+                winner: 'X'
+            });
+        } else if(rezult == 'O'){
+            this.setState({
+                gameEnded: true,
+                winner: 'O'
+            });
+        }
+
+    }
+
+    checkWinner(){
+        var moves = [[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6],[0,1,2],[3,4,5],[6,7,8]];
+        var board = this.state.board;
+        for(let i=0;i<moves.length;i++){
+            if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]]){
+                return board[moves[i][0]];
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div id="game">
+                <div id="head">
+                    Tic Tac Toe
+                </div>
+                <div id="board" onClick={(e)=>this.clicked(e)}>
+                    <div className="square" data-square="0"></div>
+                    <div className="square" data-square="1"></div>
+                    <div className="square" data-square="2"></div>
+                    <div className="square" data-square="3"></div>
+                    <div className="square" data-square="4"></div>
+                    <div className="square" data-square="5"></div>
+                    <div className="square" data-square="6"></div>
+                    <div className="square" data-square="7"></div>
+                    <div className="square" data-square="8"></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
